6ba5c0eb528e964b5381d7b68f2dca4ef71ae256
Analyn Cajocson <analyncajocson@ymail.com>
2015-05-09 16:31:01 +0800

    contacts config

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509080554) do
+ActiveRecord::Schema.define(version: 20150509082453) do
+
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string   "fullname"
